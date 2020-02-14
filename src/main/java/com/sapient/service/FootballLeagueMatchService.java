6a33bd315d3bccb0129d858c76325ff90d1e9719
PSEH-5 <39405984+PSEH-5@users.noreply.github.com>
2020-02-14 13:08:45 +0530

    Service method updated

--- src/main/java/com/sapient/service/FootballLeagueMatchService.java
+++ src/main/java/com/sapient/service/FootballLeagueMatchService.java
@@ -1,7 +1,26 @@
 package com.sapient.service;
 
+import com.sapient.model.FootballLeagueMacthModel;
+import com.sapient.utils.FootballLeagueException;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FootballLeagueMatchService {
+    public List<FootballLeagueMacthModel> getLeagueDetails(Integer countryId, Integer matchId, Optional<Integer> position)
+
+    throws FootballLeagueException {
+
+        if(countryId == null || matchId == null){
+            throw new FootballLeagueException("Country or Match doesnot exist");
+        }
+        if(position.isPresent()){
+
+        }
+
+        return null;
+    }
 }
