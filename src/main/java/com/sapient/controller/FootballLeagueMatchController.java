c0e901f2979baf8b7f73d5cc92f605b742199afd
PSEH-5 <39405984+PSEH-5@users.noreply.github.com>
2020-02-14 13:08:03 +0530

    method update

--- src/main/java/com/sapient/controller/FootballLeagueMatchController.java
+++ src/main/java/com/sapient/controller/FootballLeagueMatchController.java
@@ -1,8 +1,23 @@
 package com.sapient.controller;
 
 
+import com.sapient.model.FootballLeagueMacthModel;
+import com.sapient.service.FootballLeagueMatchService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FootballLeagueMatchController {
+    @Inject
+    FootballLeagueMatchService footballLeagueMatchService;
+
+    @GetMapping()
+    List<FootballLeagueMacthModel> getLeagueDetails(@RequestParam Integer countryId, @RequestParam Integer matchId, @RequestParam Optional<Integer> position){
+        return footballLeagueMatchService.getLeagueDetails(countryId,matchId,position);
+    }
 }
